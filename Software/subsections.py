f2356c9921231325cbc7ce2eb6c0255eee896cd7
jackamsbry <60232817+jackamsbry@users.noreply.github.com>
2020-03-31 15:47:23 -0400

    reworked hexleg class

--- Software/subsections.py
+++ Software/subsections.py
@@ -97,53 +97,48 @@ class body(object):
 
 class hexleg(object):
     """object to calculate inverse kinematics for 3dof hexapod leg and control servo motion"""
-    def __init__(self, legIndex, a_coxa, a_femur, a_tibia):
+    #Leg Section lengths in mm
+    TIBIA_LENGTH = 193.68
+    FEMUR_LENGTH = 127.47
+    COXA_LENGTH = 83
+
+    def __init__(self, legIndex, ServoKit, isInverted=False):
         self.legIndex = legIndex
-        
+        #Set initial values for robot startup
         self.a_coxa = 90
         self.a_femur = 0
         self.a_tibia = 90
-        self.angles = [self.a_coxa, self.a_femur, self.a_tibia]
-        self.x = 0
-        self.y = 0
-        self.z = 0
-        self.forwardKin(a_coxa, a_femur, a_tibia)
-
-        #Parameters for kinematic model
-        #length of leg limbs in inches
-        self.l_tibia = 7.1
-        self.l_femur = 5.2
-        self.l_coxa = 3.4
-
-        if self.legIndex <= 2:
-            self.isInverted = False
-            servo_address = 0x60
-            self.servos = [legIndex * 3, legIndex*3 + 1, legIndex*3 + 2]
+        self.isInverted = isInverted
+        
+        if not isInverted:
+            self.servo_index = [self.legIndex*3, self.legIndex*3 + 1, self.legIndex*3 + 2]
         else:
-            self.isInverted = True
-            servo_address = 0x40
-            self.servos = [(legIndex - 3)*3, (legIndex - 3)*3 + 1, (legIndex - 3)*3 + 2]
-        if self.isInverted:
-            for ang in range(len(self.angles)):
-                self.angles[ang] = 180 - self.angles[ang]
-        else:
-            pass
+            self.servo_index = [(self.legIndex-3)*3, (self.legIndex-3)*3 + 1, (self.legIndex-3)*3 + 2]
 
-        self.kit = ServoKit(channels=16, address=servo_address) 
+        self.coxa_servo = ServoKit.servo[self.servo_index[0]]
+        self.femur_servo = ServoKit.servo[self.servo_index[1]]
+        self.tibia_servo = ServoKit.servo[self.servo_index[2]]
 
-    def forwardKin(self, a_coxa, a_femur, a_tibia):
+        #Initialize variables for inverse kinematics
         self.x = 0
         self.y = 0
         self.z = 0
 
+        self.x_new = 0
+        self.y_new = 0
+        self.z_new = 0
+
+        self.run_coxa_angle(self.a_coxa)
+        self.run_femur_angle(self.a_femur)
+        self.run_tibia_angle(self.a_tibia)
 
     def inverse_kin(self, x, y, z):
         #equations taken from blog post by user downeym here: https://www.robotshop.com/community/forum/t/inverse-kinematic-equations-for-lynxmotion-3dof-legs/21336
         legLength = math.sqrt((x**2) + (y**2))
-        HF = math.sqrt((legLength - self.l_coxa)**2 + (self.z**2))
-        A1 = math.degrees(math.atan2(legLength-self.l_coxa, self.z))
-        A2 = math.degrees(math.acos((self.l_tibia**2 - self.l_femur**2 - HF**2)/(-2* self.l_femur* HF)))
-        B1 = math.degrees(math.acos((HF**2 - self.l_tibia**2 - self.l_femur**2)/(-2* self.l_femur* self.l_tibia)))
+        HF = math.sqrt((legLength - COXA_LENGTH)**2 + (self.z**2))
+        A1 = math.degrees(math.atan2(legLength - COXA_LENGTH, self.z))
+        A2 = math.degrees(math.acos((TIBIA_LENGTH**2 - FEMUR_LENGTH**2 - HF**2)/(-2* FEMUR_LENGTH * HF)))
+        B1 = math.degrees(math.acos((HF**2 - TIBIA_LENGTH**2 - FEMUR_LENGTH**2)/(-2* FEMUR_LENGTH * TIBIA_LENGTH)))
 
         a_tibia = 90 - B1 + 90
         a_femur = 90 - (A1 + A2) + 90
@@ -155,53 +150,40 @@ class hexleg(object):
 
         return a_tibia, a_femur, a_coxa
 
-    def run_angle(self, move_angle, servo_index):
+    def run_coxa_angle(self, move_angle):
         """Runs to a target angle for a given servo"""
         if self.isInverted:
             new_angle = 180 - move_angle
         else:
             new_angle = move_angle
-        current_angle = self.angles[servo_index]
-        run_servo = self.servos[servo_index]
-        print("Running Angle!")
-        for ang in np.linspace(current_angle, new_angle, 100):
-            ang = float(ang)
-            self.kit.servo[run_servo].angle = ang
-            time.sleep(0.001)
-        self.servos[servo_index] = new_angle
 
-    def interpolate_move(self, move_position, numSteps):
-        """interpolates between servos for smooth movement using cubic spline intrpolation"""
-        current_a_coxa = self.a_coxa
-        current_a_femur = self.a_femur
-        current_a_tibia = self.a_tibia
-        new_x = move_position[0]
-        new_y = move_position[1] 
-        new_z = move_position[2]
-        a_tibia, a_femur, a_coxa = self.inverse_kin(new_x, new_y, new_z)
-        a_coxa_move = np.linspace(current_a_coxa, a_coxa, numSteps)
-        a_femur_move = np.linspace(current_a_femur, a_femur, numSteps)
-        a_tibia_move = np.linspace(current_a_tibia, a_tibia, numSteps)
-        angle_moves = [a_coxa_move, a_femur_move, a_tibia_move]
+        self.coxa_servo.angle = new_angle
+
+    def run_femur_angle(self, move_angle):
+        """Runs to a target angle for a given servo"""
+        if self.isInverted:
+            new_angle = 180 - move_angle
+        else:
+            new_angle = move_angle
+
+        self.coxa_servo.angle = new_angle
+
+    def run_tibia_angle(self, move_angle):
+        """Runs to a target angle for a given servo"""
+        if self.isInverted:
+            new_angle = 180 - move_angle
+        else:
+            new_angle = move_angle
+
+        self.tibia_servo.angle = new_angle
+
+
+    def interpolate_time(self, speed, numSteps):
+        """interpolates between two delay times for smooth movement using cubic spline intrpolation"""
         time_steps = [0, numSteps/2, numSteps]
-        delay_time = [0.005, 0.0001, 0.005]
+        delay_time = [0.005*speed, 0.01*speed, 0.005*speed]
         delayInterpolation = CubicSpline(time_steps, delay_time)
         delay_time = []
         for n in range(numSteps):
             delay_time.append(delayInterpolation(n))
-        return delay_time, angle_moves
-
-    def run_position(self, move_position):
-        
-        coxa = self.servos[0]
-        femur = self.servos[1]
-        tibia = self.servos[2]
-        delay_times, angle_moves = self.interpolate_move(move_position, 200)
-        coxa_angles = angle_moves[0]
-        femur_angles = angle_moves[1]
-        tibia_angles = angle_moves[2]
-        for step in range(len(delay_times)):
-            self.kit.servo[coxa].angle = coxa_angles[step]
-            self.kit.servo[femur].angle = femur_angles[step]
-            self.kit.servo[tibia].angle = tibia_angles[step]
-            time.sleep(delay_times[step])
+        return delay_time
